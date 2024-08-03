64039f8fa00f76ea42a062ce7b43540e9126c477
Luis <luispmv07@hotmail.com>
2024-08-02 21:59:05 -0600

    Agregando la funcionalidad de crear una nueva etiqueta li cada vez que el usuario coloque un valor en el input y lo mande presionando el boton

--- app.js
+++ app.js
@@ -4,11 +4,29 @@ const taskForm = document.getElementById("task-form");
 //Nos traemos la lista desordenada ul con el id #task-list
 const taskList = document.getElementById("task-list");
 
-//Creamos un evento submit el cual ejecuta una funcion anonima la cual toma como parametro event, dentro de la funcion prevenimos el comportamiento default de el boton submit para que no se refresque la pagina.
-//Tambien dentro de la funcion llamamos al input dentro del form con id #task-input y guardamos el valor de este en la constante task para despues imprimirlo por consola
+function createNewListItem(task){
+    new_li = document.createElement("li")
+    new_li.textContent = `${task}`
+
+    new_first_span = document.createElement("span")
+    new_first_span.setAttribute("class", "edit-btn")
+    new_first_span.textContent = "✏️"
+
+    new_second_span = document.createElement("span")
+    new_second_span.setAttribute("class","delete-btn")
+    new_second_span.textContent = "❌"
+
+    new_li.append(new_first_span)
+    new_li.append(new_second_span)
+    return new_li
+}
+
+
 taskForm.addEventListener("submit", (event)=>{
     event.preventDefault()
     const taskInput = document.getElementById("task-input")
     const task = taskInput.value;
-    console.log(task)
+    // console.log(task)
+    nuevo_item = createNewListItem(task)
+    taskList.append(nuevo_item)
 })
